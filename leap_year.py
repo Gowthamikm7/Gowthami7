a7a828b084badf39e90dec194b5d4a52961d9451
Gowthami KM <gowthamikmgowda703@gmail.com>
2024-07-25 17:44:00 +0530

    Update leap_year.py

--- leap_year.py
+++ leap_year.py
@@ -1,3 +1,10 @@
+# A year is a leap year if:
+# It is divisible by 4.
+# If it is also divisible by 100, it must be divisible by 400 to be a leap year.
+# If a year is not divisible by 4, it is not a leap year.
+# If a year is divisible by 4 but not by 100, it is a leap year.
+# If a year is divisible by 4 and by 100, it must also be divisible by 400 to be a leap year.
+
 year = int(input("Enter the year "))
 if year %4 !=0:
  print("Not leap year")
@@ -9,9 +16,4 @@ else:
  else:
   print("Not leap year")
 
-# A year is a leap year if:
-# It is divisible by 4.
-# If it is also divisible by 100, it must be divisible by 400 to be a leap year.
-# If a year is not divisible by 4, it is not a leap year.
-# If a year is divisible by 4 but not by 100, it is a leap year.
-# If a year is divisible by 4 and by 100, it must also be divisible by 400 to be a leap year.
+
